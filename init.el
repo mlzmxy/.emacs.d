cf4d417db7e646e13cc518404fc0ffb939d4bf93
mlzmxy <mlzmxy@139.com>
2018-07-24 21:45:22 +0800

    add magit

--- init.el
+++ init.el
@@ -14,9 +14,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (window-numbering yasnippet linum-relative helm company))))
+    (magit ghub magit-popup window-numbering yasnippet linum-relative helm company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- site-lisp/config/mlzmxy_emacs_keybinding.el
+++ site-lisp/config/mlzmxy_emacs_keybinding.el
@@ -5,3 +5,6 @@
 (global-set-key (kbd "M-l") 'forward-char)  ; was downcase-word
 (global-set-key (kbd "M-i") 'previous-line) ; was tab-to-tab-stop
 (global-set-key (kbd "M-k") 'next-line) ; was kill-sentence
+
+;; magit keys
+(global-set-key (kbd "C-x g") 'magit-status)
