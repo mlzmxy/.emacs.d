e35eaf5a0a13f776ebae09edd48cb1613c150163
mlzmxy <mlzmxy@139.com>
2018-07-24 21:51:59 +0800

    add extension files

---
+++ elpa/dash-20180413.30/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (23383 9715 518875 999000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
