94736907ed4e782abfd8fd9e0694f48fea083ae3
mlzmxy <mlzmxy@139.com>
2018-07-24 20:10:27 +0800

    delete old files

--- lisp/mlzmxy_emacs_keybinding.el
+++
@@ -1,7 +0,0 @@
-;; Emacs Keybinding
-
-;; make cursor movement keys under right hand's home-row.
-(global-set-key (kbd "M-j") 'backward-char) ; was indent-new-comment-line
-(global-set-key (kbd "M-l") 'forward-char)  ; was downcase-word
-(global-set-key (kbd "M-i") 'previous-line) ; was tab-to-tab-stop
-(global-set-key (kbd "M-k") 'next-line) ; was kill-sentence
